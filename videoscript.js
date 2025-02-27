97a26d47d07ef87f76b483ae15d31ac1715fe305
Fredrik <fredrik@rickebasta.se>
2025-02-27 14:40:17 +0100

    changes the video player to video.js

---
+++ videoscript.js
@@ -0,0 +1,24 @@
+var videoElement = document.getElementById("video");
+var player = videojs(videoElement, {
+  liveui: true,
+  controlBar: {
+    liveDisplay: true,
+    seekBar: false,
+    progressControl: false,
+    currentTimeDisplay: false,
+    durationDisplay: false,
+    remainingTimeDisplay: false,
+  },
+});
+
+if (Hls.isSupported()) {
+  var hls = new Hls();
+  hls.loadSource("https://hydroponics.ntig.dev/hls/stream.m3u8");
+  hls.attachMedia(videoElement);
+  hls.on(Hls.Events.MANIFEST_PARSED, function () {
+    videoElement.play();
+  });
+} else if (videoElement.canPlayType("application/vnd.apple.mpegurl")) {
+  videoElement.src = "https://hydroponics.ntig.dev/hls/stream.m3u8";
+  videoElement.play();
+}
